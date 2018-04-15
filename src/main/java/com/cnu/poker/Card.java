4db8127c7ccdd143997cab9985e6d4ecab68e3c0
david choi <dachoi@riotgames.com>
2018-04-15 19:12:00 +0900

    Add card for blackjack:

--- src/main/java/com/cnu/poker/Card.java
+++ src/main/java/com/cnu/poker/Card.java
@@ -1,4 +1,17 @@
 package com.cnu.poker;
 
+import lombok.Data;
+
+@Data
 public class Card {
+    private final int rank;
+    private final Suit suit;
+
+    public Card(int rank, Suit suit) {
+        if (rank > 13) {
+            throw new NoSuchRankException();
+        }
+        this.rank = rank;
+        this.suit = suit;
+    }
 }
---
+++ src/main/java/com/cnu/poker/Suit.java
@@ -0,0 +1,5 @@
+package com.cnu.poker;
+
+public enum Suit {
+    SPADES, HEARTS, CLUBS, DIAMONDS;
+}
